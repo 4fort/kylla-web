f6b254cf4c727e9f9518b8101a70067fec751e9b
Dexter Fort <dexterkun16@gmail.com>
2024-02-04 01:08:58 +0800

    updated

--- main.js
+++ main.js
@@ -56,25 +56,70 @@
 //   noButton.style.transform = `translate(${x}px, ${y}px)`;
 // };
 
+const heroText = `
+<div class="opening-text fade">
+            <div class="qt-open">“</div>
+            <div class="text">
+              <h1>Hello,</h1>
+              <h1>Allyssa</h1>
+            </div>
+            <div class="qt-close">”</div>
+          </div>`;
+
+const navigator = document.querySelector(".navigator");
+const navLeft = document.querySelector(".nav-left");
+const navRight = document.querySelector(".nav-right");
+const navIndicator = document.querySelector(".nav-indicator");
+const navIndicatorTabs = document.querySelector(".indicator");
+
 const wrapper = document.querySelector(".wrapper");
 const sectionFirst = document.querySelector(".section-first");
+sectionFirst.innerHTML = heroText;
 const openingText = document.querySelector(".opening-text");
 const mainText = document.querySelector(".text");
+
 let isClickable = false;
 let page = 0;
 
 const qtLineOne = `
   <p class="one">One day,</p>
-  <p class="two">I'm gonna have what I prayed for.</p>
+  <p class="two">I'm gonna have what I prayed for...</p>
   <p class="three">Now I do.</p>`;
 
-// const qtLineTwo = `
-//   <p class="one">Your laugh is a music to my ears.</p>
-//   <p class="two">You don't know how much I really wanted</p>
-//   <p class="two">to take a video of you every time you genuinely laugh,</p>
-//   <p class="two">I just couldn't,</p>
-//   <p class="three">taking out my phone will ruin the moment.</p>
-// `;
+const letters = [
+  `
+  <p>
+  I'm not really good in writing letters and thinking of something
+  to start this took me a long time.
+</p>
+<p>
+  I usually get annoyed when someone talks too much but I never got
+  annoyed not even once when u do.
+</p>
+<p>
+  You are the person that no one would really hate; There is
+  literally nothing to hate about you. You deserve pure love and
+  mwahmwah hahah.
+</p>
+<p>
+  I just want to say that I'm so happy to meet and know you.
+  Ever since we met my life has been very different; I like this way
+  better.
+</p>
+  `,
+  `
+  <p>
+  Your laugh is a music to my ears. You don't know how much I really
+  wanted to take a video of you every time you laugh, I just
+  couldn't take my phone out and ruin the pure genuine moment.
+</p>
+<p>Even though we never said it to each other, we knew</p>
+<p>
+  I have so many other things to say but I'm gonna save it for next
+  time.
+</p>
+  `,
+];
 
 const quotes = [qtLineOne];
 
@@ -83,21 +128,103 @@ const qtFirst = `
     ${quotes[page]}
   </div>`;
 
+const loveLetter = `
+<div class="letter fade">
+  <h1>Dearest Allyssa,</h1>
+  <div class="body"></div>
+</div>
+`;
+
+const pageComponents = [heroText, qtFirst, loveLetter];
+
+const updateNavIndicator = () => {
+  navIndicatorTabs.innerHTML = pageComponents
+    .map((_, i) => {
+      if (page == i) return '<div class="active"></div>';
+      return '<div class=""></div>';
+    })
+    .join("");
+};
+
 setTimeout(() => {
   openingText.style = "transform: scale(.9);";
   mainText.style = "transform: scale(1.2);";
+  navIndicator.style = "animation: fade 1s;";
+
+  updateNavIndicator();
+
   isClickable = true;
 }, 2000);
 
-wrapper.addEventListener("click", () => {
-  console.log("clicked");
+// wrapper.addEventListener("click", () => {
+//   console.log("clicked");
+//   if (isClickable) {
+//     isClickable = false;
+
+//     if (page == 0) {
+//       sectionFirst.innerHTML = qtFirst;
+//       page++;
+//     } else if (page == 1) {
+//       sectionFirst.innerHTML = loveLetter;
+//     }
+
+//     setTimeout(() => {
+//       if (page == 0)
+//         document.querySelector(".quotes").style = "transform: scale(1.08);";
+//       isClickable = true;
+//     }, 2000);
+//   }
+// });
+
+const updatePage = () => {
+  console.log(page);
+  updateNavIndicator();
+
+  switch (page) {
+    case 0:
+      sectionFirst.innerHTML = heroText;
+      break;
+    case 1:
+      sectionFirst.innerHTML = qtFirst;
+      break;
+    case 2:
+      sectionFirst.innerHTML = loveLetter;
+      showLetter();
+      break;
+  }
+};
+
+navLeft.addEventListener("click", () => {
   if (isClickable) {
     isClickable = false;
-    sectionFirst.innerHTML = qtFirst;
-
+    if (page <= pageComponents.length - 1 && page > 0) page--;
+    updatePage();
     setTimeout(() => {
-      document.querySelector(".quotes").style = "transform: scale(1.08);";
       isClickable = true;
-    }, 1500);
+    }, 2000);
   }
 });
+
+navRight.addEventListener("click", () => {
+  if (isClickable) {
+    isClickable = false;
+    if (page < pageComponents.length - 1) page++;
+    updatePage();
+    setTimeout(() => {
+      isClickable = true;
+    }, 2000);
+  }
+});
+
+const showLetter = () => {
+  const letter = document.querySelector(".letter");
+  const letterBody = document.querySelector(".letter .body");
+
+  setTimeout(() => {
+    letterBody.innerHTML = letters[0];
+    letterBody.style = "animation: fade 1.5s;";
+    letter.style = `
+      max-height: ${letterBody.offsetHeight}px; 
+      `;
+  }, 2000);
+};
